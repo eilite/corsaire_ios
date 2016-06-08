8b39fe321b240a3e2a295e553793a089de3e9efd
Poussou Elie <poussoueli@eisti.eu>
2016-06-08 22:58:50 +0200

    prepared navigation page

---
+++ Corsaire/ValidNavigationController.swift
@@ -0,0 +1,22 @@
+//
+//  ValidNavigationController.swift
+//  Corsaire
+//
+//  Created by Elie on 08/06/2016.
+//  Copyright © 2016 Elie. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+
+class ValidNavigationController: UIViewController{
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var directionImage: UIImageView!
+    @IBOutlet weak var directionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        
+    }
+}
